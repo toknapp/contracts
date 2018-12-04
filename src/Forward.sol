a9ad9e0913f999988b4c738deb2af26c90da6b38
Gustav Behm <gustav@upvest.co>
2018-12-04 17:16:29 +0100

    Add basic Solidity contracts and their Makefile

---
+++ src/Forward.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+contract Forward {
+    function forward(address target, uint256 v, bytes i)
+        public payable returns (bool) {
+            // TODO: verify signature
+            // TODO: verify nonce (maybe include tx originator and tx nonce in the signature?)
+            // TODO: increase nonce (then this would be unnecessary, and there's no need to have a separate call to get the nonce)
+            // TODO: handle output data? maybe annoying to do in solidity
+            return target.call.value(v)(i);
+        }
+}
