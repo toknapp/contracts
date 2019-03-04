1bce74dbbb040ceef135933e98bdcbcea4d6dc84
Gustav Behm <gustav@upvest.co>
2019-03-04 12:45:40 +0100

    Add sketch of a forwarding contract test suite

---
+++ python/test_forward.py
@@ -0,0 +1,31 @@
+import unittest
+
+class UseCaseTests(unittest.TestCase):
+    @unittest.skip('not implemented')
+    def test_receive_ether(self):
+        self.fail()
+
+    @unittest.skip('not implemented')
+    def test_send_ether(self):
+        self.fail()
+
+    @unittest.skip('not implemented')
+    def test_transfer_erc20(self):
+        self.fail()
+
+    @unittest.skip('not implemented')
+    def test_return_value(self):
+        self.fail()
+
+class SecurityTests(unittest.TestCase):
+    @unittest.skip('not implemented')
+    def test_reject_invalid_signature(self):
+        self.fail()
+
+    @unittest.skip('not implemented')
+    def test_reject_incorrect_nonce(self):
+        self.fail()
+
+    @unittest.skip('not implemented')
+    def test_reject_incorrect_contract_address(self):
+        self.fail()
