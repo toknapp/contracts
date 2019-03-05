1e245dc5c4a211f12fe3d44d06c9920f6c8276c6
Gustav Behm <gustav@upvest.co>
2019-03-05 17:19:04 +0100

    Return the correct answer: 42

--- pycontracts/tests/test_further.py
+++ pycontracts/tests/test_further.py
@@ -3,6 +3,14 @@ import unittest
 from pycontracts.tests.test_settings import *
 
 class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.contract = deploy(contracts['further'])
+
     def test_deploy(self):
-        contract = deploy(contracts['further'])
-        self.assertGreater(len(w3.eth.getCode(contract.address)), 1)
+        self.assertGreater(len(w3.eth.getCode(self.contract.address)), 1)
+
+    def test_call(self):
+        res = w3.eth.call({
+            'to': self.contract.address,
+        })
+        self.assertEqual(int.from_bytes(res, 'big'), 42)
