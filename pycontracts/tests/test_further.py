4bdaed5732a5d053080dc0e252713931e000d5ae
Gustav Behm <gustav@upvest.co>
2019-03-05 19:04:56 +0100

    Return the correct answer (42) set in storage while deploying contract

--- pycontracts/tests/test_further.py
+++ pycontracts/tests/test_further.py
@@ -7,10 +7,11 @@ class BasicTests(unittest.TestCase):
         self.contract = deploy(contracts['further'])
 
     def test_deploy(self):
-        self.assertGreater(len(w3.eth.getCode(self.contract.address)), 1)
+        self.assertEqual(
+            w3.eth.getCode(self.contract.address),
+            contracts['further']['body']
+        )
 
     def test_call(self):
-        res = w3.eth.call({
-            'to': self.contract.address,
-        })
+        res = w3.eth.call({ 'to': self.contract.address, })
         self.assertEqual(int.from_bytes(res, 'big'), 42)
